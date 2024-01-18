fdf11cafbfb240f24e3fcf48bbcce43ba9db24de
MariusSilviuHriscu <hriscumarius65536@gmail.com>
2024-01-18 16:32:04 +0200

    working on impriving the marketplace managers

--- the_west_inner/movement_manager.py
+++ the_west_inner/movement_manager.py
@@ -1,6 +1,5 @@
 import typing
 
-from player_data import Player_data
 from towns import Town
 from task_queue import TaskQueue
 from work_manager import Work_manager
@@ -24,10 +23,17 @@ class MovementManager:
         self.task_queue = task_queue
         self.work_manager = work_manager
         self.player_data = player_data
+        self.map : Map = Map(handler = self.handler)
     def check_location(self,target_x : int , target_y : int) -> bool:
-        return self.player_data.x,self.player_data.y == target_x,target_y
-    def move_to_town(self,town : Town) -> int:
-        if self.check_location(target_x=town.x,target_y=town.y):
+        return self.player_data.x == target_x and self.player_data.y == target_y
+    def move_to_town(self,town:int|Town):
+        if isinstance(town,Town):
+            self._move_to_town(town=town)
+        elif isinstance(town,int) or isinstance(town,str):
+            town = self.map.towns[town]
+            self._move_to_town(town = town)
+    def _move_to_town(self,town : Town) -> int:
+        if self.check_location(target_x = town.x , target_y=town.y):
             return town.town_id
         self.work_manager.move_to_town(town_id = town.town_id)
         wait_until_date(wait_time= self.task_queue.get_tasks_expiration(), handler = self.handler)
@@ -35,8 +41,7 @@ class MovementManager:
         return town.town_id
     def move_to_closest_town(self) :
         
-        map = Map(handler = self.handler)
-        town_list = map.towns
+        town_list = self.map.towns
         closest_town = town_list.get_closest_town(player_data = self.player_data)
         return self.move_to_town(town = closest_town)
     @staticmethod

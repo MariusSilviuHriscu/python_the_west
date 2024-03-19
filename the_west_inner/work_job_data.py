af54ac50d8ee23ea90d40fd0b78ace6836247d91
MariusSilviuHriscu <hriscumarius65536@gmail.com>
2024-03-19 22:16:30 +0200

    work_data filtering rules

--- the_west_inner/work_job_data.py
+++ the_west_inner/work_job_data.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 
 from requests_handler import requests_handler
 from player_data import Player_data
-from map import Map
+from map import Map, MapLoader
 
 @dataclass
 class WorkData:
@@ -22,7 +22,7 @@ class JobDataDamage:
     damage_max_damage : int
     
     def average_damage(self , max_hp : int) ->int:
-        return self.damage_max_damage * self.damage_probability * max_hp
+        return self.damage_max_damage * self.damage_probability / 100 * max_hp
 class WorkJobData:
     def __init__(self,
                  work_id : int ,
@@ -54,7 +54,6 @@ class WorkJobData:
             'x' : x,
             'y' : y
         }
-        
         response = handler.post(window='job',action='job',action_name='mode',payload=payload)
         
         
@@ -73,7 +72,6 @@ class WorkJobData:
     def load_joab_damage(self, handler : requests_handler , x : int , y : int) -> JobDataDamage:
         
         if self.job_data is None:
-            
             self._load_job_data(
                 handler= handler,
                 x = x ,
@@ -88,9 +86,124 @@ class WorkJobData:
         
         return job_damage
 
-class WorkDataLoader():
-    def __init__(self, handler : requests_handler ):
+class WorkSortRule(Protocol):
+    def __init__(self) -> None:
+        pass
+    
+    def value(self,work_data : WorkJobData) -> int:
+        pass
+class WorkValidationRule(Protocol):
+    def __init__(self) -> None:
+        pass
+    
+    def validate(self,work_data : WorkJobData) -> bool:
+        pass
+
+class DamageValidationRule():
+    def __init__(self,
+                 handler:requests_handler,
+                 player_data : Player_data,
+                 map : Map ,
+                 max_allowed_damage_percent: float
+                 ):
         self.handler = handler
+        self.player_data = player_data
+        self.map = map
+        self.max_allowed_damage_percent = max_allowed_damage_percent
+    def _max_damage(self) ->int:
+        
+        return int(self.player_data.hp_max * self.max_allowed_damage_percent)
+    def validate(self,work_data : WorkJobData) -> bool:
+        
+        work_map_obj = self.map.job_location_data.get_random_job(job_id = work_data.work_id)
+        
+        work_data.load_joab_damage(handler=self.handler,
+                                   x = work_map_obj.job_x,
+                                   y = work_map_obj.job_y
+                                   )
+        return work_data.job_data_damage().average_damage(max_hp = self.player_data.hp_max) < self._max_damage()
+class FrequencyDamageValidationRule():
+    def __init__(self,
+                 handler:requests_handler,
+                 player_data : Player_data,
+                 map : Map ,
+                 max_allowed_damage_frequency: float
+                 ):
+        self.handler = handler
+        self.player_data = player_data
+        self.map = map
+        self.max_allowed_damage_frequency = max_allowed_damage_frequency
+    def validate(self,work_data : WorkJobData) -> bool:
+        
+        work_map_obj = self.map.job_location_data.get_random_job(job_id = work_data.work_id)
+        
+        work_data.load_joab_damage(handler=self.handler,
+                                   x = work_map_obj.job_x,
+                                   y = work_map_obj.job_y
+                                   )
+        
+        return work_data.job_data_damage().damage_probability  < self.max_allowed_damage_frequency
+        
+class ExpDamageRule():
+    
+    def __init__(self, time_value : typing.Literal[15,60,3600]):
+        self.time_value = time_value
+    
+    def value(self , work_data : WorkJobData):
+        
+        work : WorkData = work_data.timed_work_data[self.time_value]
+        return work.xp
+class WorkJobDataManager:
+    
+    def __init__(self , 
+                 player_data : Player_data ,
+                 work_job_data_list : list[WorkJobData] ,
+                 map : Map
+                 ) :
+        self.player_data = player_data
+        self.map = map
+        self.work_job_data_list = work_job_data_list
+        
+        self.work_dict = {x.work_id : x for x in work_job_data_list}
+    
+    def sort_rule(self,work_sort_rules:list[WorkSortRule]|WorkSortRule) -> typing.Callable[[WorkJobData],int]:
+        def get_value_func(work_job_data : WorkJobData | list[WorkJobData]) -> int:
+            if isinstance(work_sort_rules , list):
+                return sum((work_sort_rule.value(work_data = work_job_data) for work_sort_rule in work_sort_rules))
+            return work_sort_rules.value(work_data=work_job_data)
+        return get_value_func
+    def filter_rule(self,filter_rules : list[WorkValidationRule] | WorkValidationRule) -> typing.Callable[[WorkJobData],bool]:
+        
+        def get_filter_func(work_job_data : WorkJobData | list[WorkJobData]) -> bool:
+            if isinstance(filter_rules,list):
+                return all((validation_rule.validate(work_data=work_job_data) for validation_rule in filter_rules))
+            return filter_rules.validate(work_data=work_job_data)
+        
+        return get_filter_func
+        
+    def sort_by_work_sort_rule(self,
+                               work_sort_rules : list[WorkSortRule],
+                               filter_rules : list[WorkValidationRule]| None = None,
+                               max_number:typing.Optional[int] = None) -> list[WorkJobData]:
+        work_list = []
+        
+        for work_data in sorted(self.work_job_data_list,key = self.sort_rule(work_sort_rules=work_sort_rules),reverse= True):
+            if filter_rules is not None and work_data.work_points >= 0 and self.filter_rule(filter_rules=filter_rules)(work_data):
+                work_list.append(work_data)
+            if max_number is not None and len(work_list) >= max_number:
+                return work_list
+        
+        return work_list
+
+    def get_by_id(self,item_id : int) -> WorkJobData:
+        
+        return self.work_dict.get(item_id)
+
+class WorkDataLoader():
+    def __init__(self, handler : requests_handler,player_data : Player_data,map : Map ):
+        self.handler = handler
+        self.player_data = player_data
+        self.map = map
     def _load_work_data(self , work_data : dict) -> WorkData:
         return WorkData(
             cost = work_data.get('cost'),
@@ -114,74 +227,17 @@ class WorkDataLoader():
                                                     for x in work_time_info}
         )
         
-    def get_work_info(self,handler:requests_handler) -> list[WorkJobData]:
+    def get_work_info(self) -> list[WorkJobData]:
         
-        response = handler.post(window="work", action="index", action_name="mode")['jobs']
+        response = self.handler.post(window="work", action="index", action_name="mode")['jobs']
         
         return [self._load_work_info(job_id = job_id,
                                      job_dict = job_dict
                                      ) for job_id,job_dict in response.items()]
-class WorkSortRule(Protocol):
-    def __init__(self) -> None:
-        pass
-    
-    def value(self,work_data : WorkJobData) -> int:
-        pass
-class WorkValidationRule(Protocol):
-    def __init__(self) -> None:
-        pass
-    
-    def validate(self,work_data : WorkJobData) -> bool:
-        pass
-
-class DamageValidationRule():
-    def __init__(self,
-                 handler:requests_handler,
-                 player_data : Player_data,
-                 map : Map ,
-                 max_allowed_damage_percent: float):
-        self.handler = handler
-        self.player_data = player_data
-        self.map = map
-        self.max_allowed_damage_percent = max_allowed_damage_percent
-    def _max_damage(self) ->int:
+    def get_work_data_manager(self) -> WorkJobDataManager:
         
-        return int(self.player_data.hp_max * self.max_allowed_damage_percent)
-    def validate(self,work_data : WorkJobData) -> bool:
-        
-        work_map_obj = self.map.job_location_data.get_random_job(job_id = work_data.work_id)
-        
-        work_data.load_joab_damage(handler=self.handler,
-                                   x = work_map_obj.job_x,
-                                   y = work_map_obj.job_y
-                                   )
-        return work_data.job_data_damage().average_damage(max_hp = self.player_data.hp_max) < self._max_damage()
-class ExpDamageRule():
-    
-    def __init__(self, time_value : typing.Literal[15,60,3600]):
-        self.time_value = time_value
-    
-    def value(self , work_data : WorkJobData):
-        
-        work : WorkData = work_data.timed_work_data[self.time_value]
-        return work.xp
-class WorkJobDataManager:
-    
-    def __init__(self , 
-                 player_data : Player_data ,
-                 work_job_data_list : list[WorkJobData] ,
-                 map : Map
-                 ) :
-        self.player_data = player_data
-        self.map = map
-        self.work_job_data_list = work_job_data_list
-        
-        self.work_dict = {x.work_id : x for x in work_job_data_list}
-    
-    def sort_by_work_sort_rule(self, work_sort_rule : WorkSortRule,filter_rule : WorkValidationRule|None):
-        
-        return sorted(
-            [x for x in self.work_job_data_list if filter_rule.validate(work_data = x)],
-            key = work_sort_rule.value ,
-            reverse= True
-            )
+        return WorkJobDataManager(
+            player_data = self.player_data,
+            work_job_data_list = self.get_work_info(),
+            map = self.map
+        )

8c9c13a6a8ec9c9046610899e098dd2872c42272
MariusSilviuHriscu <hriscumarius65536@gmail.com>
2024-03-22 23:55:16 +0200

    exp script action calculator

--- automation_scripts/exp_gain_script/exp_script.py
+++ automation_scripts/exp_gain_script/exp_script.py
@@ -7,7 +7,8 @@ from the_west_inner.work_job_data import (WorkDataLoader,
                                           WorkJobDataManager,
                                           WorkSortRule,
                                           WorkValidationRule,
-                                          WorkJobData
+                                          WorkJobData,
+                                          WorkData
                                           )
 
 
@@ -37,7 +38,96 @@ class ExpScriptJobDataManager:
             work_sort_rules = self.work_sort_rule_list,
             filter_rules = self.work_validation_rule_list,
         )
+
+
+class ExpScriptAction:
+    def __init__(self, action_number : int,duration : int , job_data : WorkJobData):
+        self.action_number = action_number
+        self.duration = duration
+        self.job_data = job_data
     
+    def calc_exp(self) -> int:
+        work_data : WorkData = self.job_data.timed_work_data[self.duration]
+        
+        return work_data.xp * self.action_number
+    
+    def calc_worktime(self) -> int:
+        
+        return self.duration * self.action_number
+
+class ExpScriptActionList:
+    def __init__(self , exp_action_list : list[ExpScriptAction]|None = None):
+        self.exp_action_list = exp_action_list
+        if exp_action_list is None:
+            self.exp_action_list = []
+    
+    def append(self,exp_action : ExpScriptAction):
+        self.exp_action_list.append(exp_action)
+    
+    def calc_exp(self) -> int:
+        
+        return sum([x.calc_exp() for x in self.exp_action_list])
+    
+    def calc_worktime(self) -> int:
+        
+        return sum([x.calc_worktime() for x in self.exp_action_list])
+
+class ExpScriptJobSelector():
+    def __init__(self,
+                 duration : int,
+                 work_data_list : list[WorkJobData],
+                 ):
+        
+        
+        self.duration = duration
+        self.work_data_list = work_data_list
+    
+    def get_possible_action_list(self , work_job_data : list[WorkJobData] , duration : int) -> ExpScriptActionList:
+        
+        job_target_data = ExpScriptActionList()
+        actions = self.player_data.energy
+        
+        for job in work_job_data:
+            
+            possible_actions = job.motivation * 100 - 75
+            
+            if possible_actions <= 0 :
+                continue
+            
+            
+            script_action = ExpScriptAction(
+                action_number = min(possible_actions , actions),
+                duration = duration,
+                job_data = job
+            )
+            
+            job_target_data.append(script_action)
+            
+            actions -= min(possible_actions , actions)
+            
+            if actions == 0:
+                
+                return job_target_data
+        return job_target_data
+    
+    def target_simulate(self, exp_target : int) -> ExpScriptActionList:
+        
+        work_list = self.work_data_list.copy()
+        work_list.reverse()
+        
+        while len(work_list) != 0:
+            
+            action_list = self.get_possible_action_list(work_job_data = work_list ,
+                                                        duration= self.duration
+                                                        )
+            
+            if action_list.calc_exp() >= exp_target:
+                return action_list
+            
+            work_list = work_list[1::]
+
+        return self.get_possible_action_list(work_job_data=self.work_data_list,duration=self.duration)
+
 class ExpScript:
     
     def __init__(self,
@@ -50,4 +140,8 @@ class ExpScript:
     
     def _get_job_data_list(self) -> list[WorkJobData]:
         
-        pass
+        return self.script_job_manager.get_filtered_job_data()
+    
+    def target_exp(self) -> int:
+        return self.player_data.required_exp
+    

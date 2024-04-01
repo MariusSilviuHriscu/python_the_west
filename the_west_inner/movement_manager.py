148e138aa2e735a7266320c5905626e1d277935c
MariusSilviuHriscu <hriscumarius65536@gmail.com>
2024-04-01 16:14:21 +0300

    documentation

--- the_west_inner/movement_manager.py
+++ the_west_inner/movement_manager.py
@@ -8,44 +8,106 @@ from requests_handler import requests_handler
 from player_data import Player_data
 
 class MovementManager:
-    
-    def __init__(self ,
-                 handler:requests_handler ,
-                 task_queue : TaskQueue,
-                 work_manager : Work_manager,
-                 player_data : Player_data
-                 ):
-        
+    """
+    Class to manage movement-related tasks for the player.
+
+    Attributes:
+        handler (requests_handler): The request handler object.
+        task_queue (TaskQueue): The task queue object.
+        work_manager (Work_manager): The work manager object.
+        player_data (Player_data): The player data object.
+    """
+    def __init__(self,
+                 handler: requests_handler,
+                 task_queue: TaskQueue,
+                 work_manager: Work_manager,
+                 player_data: Player_data):
+        """
+        Initializes the MovementManager.
+
+        Args:
+            handler (requests_handler): The request handler object.
+            task_queue (TaskQueue): The task queue object.
+            work_manager (Work_manager): The work manager object.
+            player_data (Player_data): The player data object.
+        """
         self.handler = handler
         self.task_queue = task_queue
         self.work_manager = work_manager
         self.player_data = player_data
-    def check_location(self,target_x : int , target_y : int) -> bool:
+
+    def check_location(self, target_x: int, target_y: int) -> bool:
+        """
+        Checks if the player is at the specified location.
+
+        Args:
+            target_x (int): The x-coordinate of the target location.
+            target_y (int): The y-coordinate of the target location.
+
+        Returns:
+            bool: True if the player is at the specified location, False otherwise.
+        """
         return self.player_data.x == target_x and self.player_data.y == target_y
-    def move_to_town(self,town:int|Town):
-        if isinstance(town,Town):
+
+    def move_to_town(self, town: int | Town):
+        """
+        Moves the player to the specified town.
+
+        Args:
+            town (int or Town): The ID or instance of the target town.
+
+        Returns:
+            int: The ID of the town the player moved to.
+        """
+        if isinstance(town, Town):
             return self._move_to_town(town=town)
-        elif isinstance(town,int) or isinstance(town,str):
+        elif isinstance(town, int) or isinstance(town, str):
             town = self.map.towns[town]
-            return self._move_to_town(town = town)
-    def _move_to_town(self,town : Town) -> int:
-        if self.check_location(target_x = town.x , target_y=town.y):
+            return self._move_to_town(town=town)
+
+    def _move_to_town(self, town: Town) -> int:
+        """
+        Moves the player to the specified town (internal method).
+
+        Args:
+            town (Town): The instance of the target town.
+
+        Returns:
+            int: The ID of the town the player moved to.
+        """
+        if self.check_location(target_x=town.x, target_y=town.y):
             return town.town_id
-        self.work_manager.move_to_town(town_id = town.town_id)
-        wait_until_date(wait_time= self.task_queue.get_tasks_expiration(), handler = self.handler)
-        self.player_data.update_visible_variables(handler = self.handler)
+        self.work_manager.move_to_town(town_id=town.town_id)
+        wait_until_date(wait_time=self.task_queue.get_tasks_expiration(), handler=self.handler)
+        self.player_data.update_visible_variables(handler=self.handler)
         return town.town_id
-    def move_to_closest_town(self) :
-        
+
+    def move_to_closest_town(self):
+        """
+        Moves the player to the closest town.
+
+        Returns:
+            int: The ID of the town the player moved to.
+        """
         town_list = self.map.towns
-        closest_town = town_list.get_closest_town(player_data = self.player_data)
-        return self.move_to_town(town = closest_town)
-    def get_distance_to_town(self , town : int|Town) -> int|float:
-        if isinstance(town,Town):
-            coordinates = (town.x , town.y)
-        elif isinstance(town,int) or isinstance(town,str):
+        closest_town = town_list.get_closest_town(player_data=self.player_data)
+        return self.move_to_town(town=closest_town)
+
+    def get_distance_to_town(self, town: int | Town) -> int | float:
+        """
+        Calculates the distance to the specified town.
+
+        Args:
+            town (int or Town): The ID or instance of the target town.
+
+        Returns:
+            int or float: The distance to the specified town.
+        """
+        if isinstance(town, Town):
+            coordinates = (town.x, town.y)
+        elif isinstance(town, int) or isinstance(town, str):
             map_town = self.map.towns[town]
-            coordinates = (map_town.x , map_town.y)
+            coordinates = (map_town.x, map_town.y)
         else:
             raise Exception('Invalid type of input')
         return self.player_data.absolute_distance_to(final_position=coordinates)
--- the_west_inner/quest_requirements.py
+++ the_west_inner/quest_requirements.py
@@ -3,214 +3,450 @@ import typing
 from gold_finder import parse_map_for_quest_employers
 from work_manager import Work_manager
 from requests_handler import requests_handler
-from crafting import Crafting_table,acquire_product
+from crafting import Crafting_table, acquire_product
 from items import Items
 from game_classes import Game_classes
-from marketplace_buy import Marketplace_categories,search_marketplace_category,search_marketplace_item
+from marketplace_buy import Marketplace_categories, search_marketplace_category, search_marketplace_item
 from marketplace import Marketplace_buy_manager
 
-class Quest_requirement():
-    def __init__(self,solved):
+
+class Quest_requirement:
+    """
+    Base class for representing quest requirements.
+    """
+    def __init__(self, solved: bool):
+        """
+        Initialize the Quest_requirement object.
+
+        Args:
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.solved = solved
+
     def declare_solved(self):
+        """
+        Mark the requirement as solved.
+        """
         self.solved = True
+
+
 class Quest_requirement_travel(Quest_requirement):
+    """
+    Represents a quest requirement related to travel.
+    """
     priority = 3
-    def __init__(self,x:int,y:int,employer_key:str,quest_id:int,solved:bool):
+
+    def __init__(self, x: int, y: int, employer_key: str, quest_id: int, solved: bool):
+        """
+        Initialize the Quest_requirement_travel object.
+
+        Args:
+            x (int): The x-coordinate of the target location.
+            y (int): The y-coordinate of the target location.
+            employer_key (str): The key of the quest employer.
+            quest_id (int): The ID of the quest.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.x = x
         self.y = y
         self.employer_key = employer_key
         self.quest_id = quest_id
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
+
 class Quest_requirement_item_to_hand_work_product_hourly(Quest_requirement):
+    """
+    Represents a quest requirement related to crafting a product hourly.
+    """
     priority = 1
-    def __init__(self,item_id:int,number:int,quest_id:int,solved:bool):
+
+    def __init__(self, item_id: int, number: int, quest_id: int, solved: bool):
+        """
+        Initialize the Quest_requirement_item_to_hand_work_product_hourly object.
+
+        Args:
+            item_id (int): The ID of the item to craft.
+            number (int): The number of items to craft.
+            quest_id (int): The ID of the quest.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.item_id = item_id
         self.number = number
         self.quest_id = quest_id
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
+
 class Quest_requirement_item_to_hand_work_product_seconds(Quest_requirement):
+    """
+    Represents a quest requirement related to crafting a product within a certain time frame.
+    """
     priority = 1
-    def __init__(self,item_id:int,number:int,quest_id:int,solved:bool):
+
+    def __init__(self, item_id: int, number: int, quest_id: int, solved: bool):
+        """
+        Initialize the Quest_requirement_item_to_hand_work_product_seconds object.
+
+        Args:
+            item_id (int): The ID of the item to craft.
+            number (int): The number of items to craft.
+            quest_id (int): The ID of the quest.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.item_id = item_id
         self.number = number
         self.quest_id = quest_id
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
+
 class Quest_requirement_item_to_hand_buy_from_marketplace(Quest_requirement):
+    """
+    Represents a quest requirement related to buying items from the marketplace.
+    """
     priority = 1
-    def __init__(self,item_id:int,number:int,quest_id:int,solved:bool):
+
+    def __init__(self, item_id: int, number: int, quest_id: int, solved: bool):
+        """
+        Initialize the Quest_requirement_item_to_hand_buy_from_marketplace object.
+
+        Args:
+            item_id (int): The ID of the item to buy.
+            number (int): The number of items to buy.
+            quest_id (int): The ID of the quest.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.item_id = item_id
         self.number = number
-        self.quest_id =quest_id
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        self.quest_id = quest_id
+        super().__init__(solved)
+
+
 class Quest_requirement_item_to_buy_from_city_building(Quest_requirement):
+    """
+    Represents a quest requirement related to buying items from a city building.
+    """
     priority = 1
-    def __init__(self,item_id:int,number:int,quest_id:int,solved:bool):
+
+    def __init__(self, item_id: int, number: int, quest_id: int, solved: bool):
+        """
+        Initialize the Quest_requirement_item_to_buy_from_city_building object.
+
+        Args:
+            item_id (int): The ID of the item to buy.
+            number (int): The number of items to buy.
+            quest_id (int): The ID of the quest.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.item_id = item_id
         self.number = number
         self.quest_id = quest_id
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
+
 class Quest_requirement_use_travelling_merchant_item(Quest_requirement):
+    """
+    Represents a quest requirement related to using items from the traveling merchant.
+    """
     priority = 1
-    
-    def __init__(self,item_id:int , number:int , quest_id : int , solved:bool):
-        
+
+    def __init__(self, item_id: int, number: int, quest_id: int, solved: bool):
+        """
+        Initialize the Quest_requirement_use_travelling_merchant_item object.
+
+        Args:
+            item_id (int): The ID of the item to use.
+            number (int): The number of items to use.
+            quest_id (int): The ID of the quest.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.item_id = item_id
         self.number = number
         self.quest_id = quest_id
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
+
 class Quest_requirement_item_to_get_from_other_quest(Quest_requirement):
+    """
+    Represents a quest requirement related to obtaining items from another quest.
+    """
     priority = 2
-    def __init__(self,item_id:int,item_id_reward_quest:int,quest_id:int,solved:bool):
+
+    def __init__(self, item_id: int, item_id_reward_quest: int, quest_id: int, solved: bool):
+        """
+        Initialize the Quest_requirement_item_to_get_from_other_quest object.
+
+        Args:
+            item_id (int): The ID of the item to obtain.
+            item_id_reward_quest (int): The ID of the quest that rewards the item.
+            quest_id (int): The ID of the quest.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.item_id = item_id
         self.item_id_reward_quest = item_id_reward_quest
         self.quest_id = quest_id
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
 
 class Quest_requirement_work_n_seconds(Quest_requirement):
+    """
+    Represents a quest requirement related to working for a certain duration.
+    """
     priority = 1
-    def __init__(self,work_id : int , required_work_time : int,solved:bool):
+
+    def __init__(self, work_id: int, required_work_time: int, solved: bool):
+        """
+        Initialize the Quest_requirement_work_n_seconds object.
+
+        Args:
+            work_id (int): The ID of the work.
+            required_work_time (int): The required duration of work in seconds.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.work_id = work_id
         self.required_work_time = required_work_time
-        self.solved = solved
-    
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
 
 class Quest_requirement_work_n_times(Quest_requirement):
+    """
+    Represents a quest requirement related to working for a certain number of times.
+    """
     priority = 1
-    def __init__(self,work_id : int , required_work_times : int,solved:bool):
+
+    def __init__(self, work_id: int, required_work_times: int, solved: bool):
+        """
+        Initialize the Quest_requirement_work_n_times object.
+
+        Args:
+            work_id (int): The ID of the work.
+            required_work_times (int): The required number of times to work.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.work_id = work_id
         self.required_work_times = required_work_times
-        self.solved = solved
-    
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
 
 class Quest_requirement_sell_item(Quest_requirement):
+    """
+    Represents a quest requirement related to selling an item.
+    """
     priority = 2
-    def __init__(self, item_id: int , solved : bool ):
+
+    def __init__(self, item_id: int, solved: bool):
+        """
+        Initialize the Quest_requirement_sell_item object.
+
+        Args:
+            item_id (int): The ID of the item to sell.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.item_id = item_id
-        self.solved = solved
-    
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
 
 class Quest_requirement_accept_other_quest(Quest_requirement):
+    """
+    Represents a quest requirement related to accepting another quest.
+    """
     priority = 1
-    def __init__(self , other_quest_id : int , solved : bool ):
+
+    def __init__(self, other_quest_id: int, solved: bool):
+        """
+        Initialize the Quest_requirement_accept_other_quest object.
+
+        Args:
+            other_quest_id (int): The ID of the other quest to accept.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.other_quest_id = other_quest_id
-        self.solved = solved
-    
-    def declare_solved(self):
-        return super().declare_solved()
- 
+        super().__init__(solved)
+
+
 class Quest_requirement_solve_other_quest(Quest_requirement):
+    """
+    Represents a quest requirement related to solving another quest.
+    """
     priority = 1
-    def __init__(self , other_quest_id : int , solved : bool ):
+
+    def __init__(self, other_quest_id: int, solved: bool):
+        """
+        Initialize the Quest_requirement_solve_other_quest object.
+
+        Args:
+            other_quest_id (int): The ID of the other quest to solve.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.other_quest_id = other_quest_id
-        self.solved = solved
-    
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
 
 class Quest_requirement_distribute_skill_point(Quest_requirement):
+    """
+    Represents a quest requirement related to distributing skill points.
+    """
     priority = 1
-    def __init__(self,number_of_skill_points:int, solved : bool):
-        self.solved = solved
+
+    def __init__(self, number_of_skill_points: int, solved: bool):
+        """
+        Initialize the Quest_requirement_distribute_skill_point object.
+
+        Args:
+            number_of_skill_points (int): The number of skill points to distribute.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.number_of_skill_points = number_of_skill_points
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
 
 class Quest_requirement_get_n_skill_points(Quest_requirement):
+    """
+    Represents a quest requirement related to obtaining a certain number of skill points.
+    """
     priority = 1
-    def __init__(self,target_number:int,skill_key : str, solved):
+
+    def __init__(self, target_number: int, skill_key: str, solved: bool):
+        """
+        Initialize the Quest_requirement_get_n_skill_points object.
+
+        Args:
+            target_number (int): The target number of skill points to obtain.
+            skill_key (str): The key of the skill.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.target_number = target_number
         self.skill_key = skill_key
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
+
 class Quest_requirement_get_n_attribute_points(Quest_requirement):
+    """
+    Represents a quest requirement related to obtaining a certain number of attribute points.
+    """
     priority = 1
-    def __init__(self,target_number:int,attribute_key : str, solved):
+
+    def __init__(self, target_number: int, attribute_key: str, solved: bool):
+        """
+        Initialize the Quest_requirement_get_n_attribute_points object.
+
+        Args:
+            target_number (int): The target number of attribute points to obtain.
+            attribute_key (str): The key of the attribute.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.target_number = target_number
         self.attribute_key = attribute_key
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
+
 class Quest_requirement_duel_npc(Quest_requirement):
+    """
+    Represents a quest requirement related to dueling an NPC.
+    """
     priority = 1
-    def __init__(self,quest_id:int,solved:bool):
+
+    def __init__(self, quest_id: int, solved: bool):
+        """
+        Initialize the Quest_requirement_duel_npc object.
+
+        Args:
+            quest_id (int): The ID of the quest.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.quest_id = quest_id
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
+
 class Quest_requirement_duel_quest_npc(Quest_requirement):
+    """
+    Represents a quest requirement related to dueling a quest NPC.
+    """
     priority = 3
-    def __init__(self,quest_id:int,solved = True):
+
+    def __init__(self, quest_id: int, solved: bool = True):
+        """
+        Initialize the Quest_requirement_duel_quest_npc object.
+
+        Args:
+            quest_id (int): The ID of the quest.
+            solved (bool, optional): Indicates if the requirement is solved or not. Defaults to True.
+        """
         self.quest_id = quest_id
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
 
 class Quest_requirement_equip_item(Quest_requirement):
+    """
+    Represents a quest requirement related to equipping an item.
+    """
     priority = 1
-    def __init__(self,item_id:int,solved : bool = True):
+
+    def __init__(self, item_id: int, solved: bool = True):
+        """
+        Initialize the Quest_requirement_equip_item object.
+
+        Args:
+            item_id (int): The ID of the item to equip.
+            solved (bool, optional): Indicates if the requirement is solved or not. Defaults to True.
+        """
         self.item_id = item_id
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
+
 
 class Quest_requirement_work_quest_item(Quest_requirement):
+    """
+    Represents a quest requirement related to working with a quest item.
+    """
     priority = 1
-    def __init__(self, item_id : int , work_id : int , solved : bool):
+
+    def __init__(self, item_id: int, work_id: int, solved: bool):
+        """
+        Initialize the Quest_requirement_work_quest_item object.
+
+        Args:
+            item_id (int): The ID of the quest item.
+            work_id (int): The ID of the work.
+            solved (bool): Indicates if the requirement is solved or not.
+        """
         self.item_id = item_id
         self.work_id = work_id
-        self.solved = solved
-    def declare_solved(self):
-        return super().declare_solved()
+        super().__init__(solved)
 
 
+class Quest_requirement_list:
+    """
+    Represents a list of quest requirements.
+    """
+    def __init__(self, requirement_list: list[Quest_requirement]):
+        """
+        Initialize the Quest_requirement_list object.
 
-#class UnknownQuestRequirement(Exception):
-#    pass
-#def sort_quest_requirement(requirement_dict:dict,handler:requests_handler):
-#    match requirement_dict:
-#        case []:
-#            Quest_requirement()
-#        case _ if 'jsInfo' in requirement_dict and requirement_dict['jsInfo']['type'] == "task-finish-walk":
-#            return Quest_requirement_travel_builder(requirement_dict=requirement_dict,handler=handler).build()
-#        case _ if 'jsInfo' in requirement_dict and requirement_dict['jsInfo']['type'] == "inventory_changed":
-#            return "TO DO!"
-#        case _ :
-#            raise UnknownQuestRequirement()
-
-    
-
-class Quest_requirement_list():
-    def __init__(self,requirement_list:list[Quest_requirement]):
+        Args:
+            requirement_list (list): List of quest requirements.
+        """
         self.requirement_list = requirement_list
-    
+
     @property
     def solved_requirements(self):
+        """
+        Check if all requirements in the list are solved.
+
+        Returns:
+            bool: True if all requirements are solved, False otherwise.
+        """
         return all([x.solved for x in self.requirement_list])
 
-def build_solved_quest_requirement(requirement_dict_iterables:typing.Iterable):
+
+def build_solved_quest_requirement(requirement_dict_iterables: typing.Iterable):
+    """
+    Build a list of solved quest requirements based on the given dictionary iterable.
+
+    Args:
+        requirement_dict_iterables (Iterable): Iterable containing dictionaries representing quest requirements.
+
+    Returns:
+        Quest_requirement_list: A list of solved quest requirements.
+    """
     requirements = []
     for requirement_dict in requirement_dict_iterables:
         requirements.append(Quest_requirement(solved=requirement_dict['solved']))
-    return Quest_requirement_list(requirement_list = requirements)
+    return Quest_requirement_list(requirement_list=requirements)

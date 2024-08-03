4f84a20f0c594a8a168adc8ea05e61b71c730ee9
MariusSilviuHriscu <hriscumarius65536@gmail.com>
2024-08-03 17:49:08 +0300

    more functionality for chat messages

--- automation_scripts/sleep_func_callbacks/message_chat.py
+++ automation_scripts/sleep_func_callbacks/message_chat.py
@@ -2,10 +2,16 @@ import typing
 
 from the_west_inner.chat_data_reader import StatusData, JoinedLeaveClientData , MessageData
 from the_west_inner.chat import Chat
+from the_west_inner.chat import ChatHandler
+from the_west_inner.chat_data import ChatData, ClientDataManager
+from the_west_inner.chat_data_reader import ChatDataParser
 
 from automation_scripts.sleep_func_callbacks.check_marketplace_for_item import Flag
+from the_west_inner.requests_handler import requests_handler
 
 ChatGeneratorType = typing.Generator[MessageData|StatusData|JoinedLeaveClientData, None, None]
+
+
 
 
 def send_message_to_chat(message : str , chat_generator : ChatGeneratorType , chat : Chat , flag : Flag | None = None):
@@ -31,3 +37,29 @@ def send_message_to_chat(message : str , chat_generator : ChatGeneratorType , ch
             
             if chat_entity.message_content == message:
                 return
+
+def message_chat(message : str ,handler : requests_handler, flag : Flag | None = None):
+    
+    chat_handler = ChatHandler(
+    handler = handler
+    )
+
+    client_data_manager = ClientDataManager(clients={})
+
+    chat_data = ChatData({},[],clients=client_data_manager)
+
+    chat_data_parser = ChatDataParser()
+    
+    chat = Chat(
+    chat_handler = chat_handler,
+    chat_data = chat_data,
+    chat_data_parser=chat_data_parser
+    )
+    chat_generator = chat.message_loop()
+    
+    send_message_to_chat(
+        message=message,
+        chat_generator = chat_generator,
+        chat = chat,
+        flag = flag
+    )

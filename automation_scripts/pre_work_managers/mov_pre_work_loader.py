7c0cc37a20799c361da633aafd1f9183ad5eedfa
MariusSilviuHriscu <hriscumarius65536@gmail.com>
2024-10-12 18:24:03 +0300

    work pre loader

---
+++ automation_scripts/pre_work_managers/mov_pre_work_loader.py
@@ -0,0 +1,27 @@
+
+
+
+
+from automation_scripts.pre_work_managers.mov_pre_work_managers import PreWorkMovementManager
+from the_west_inner.equipment import Equipment
+from the_west_inner.game_classes import Game_classes
+from the_west_inner.work import Work
+
+
+def create_work_target_movement(work : Work , 
+                                
+                                ):
+    pass
+
+def build_pre_work_movement_manager(
+    game_classes : Game_classes,
+    work_list : list[Work],
+    movement_equipment : Equipment
+    ) -> PreWorkMovementManager:
+    
+        
+    
+    
+    return PreWorkMovementManager(
+        
+    )

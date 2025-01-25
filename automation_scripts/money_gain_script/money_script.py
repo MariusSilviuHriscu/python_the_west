2c7186b4280bc953fa77d1a1808e30e2e4fba9bb
MariusSilviuHriscu <hriscumarius65536@gmail.com>
2025-01-25 22:50:37 +0200

    money_making_scripting

--- automation_scripts/money_gain_script/money_script.py
+++ automation_scripts/money_gain_script/money_script.py
@@ -1,25 +1,35 @@
 
 from datetime import time
 import typing
+from automation_scripts.example_scripts.work_cycle_v1 import stop_works
+from automation_scripts.sleep_func_callbacks.callback_chain import CallbackChainer
+from automation_scripts.sleep_func_callbacks.misc_func_callback import recharge_health_equipment
+from automation_scripts.work_cycle import Cycle_jobs
 from the_west_inner.equipment import Equipment
 from the_west_inner.game_classes import Game_classes
 from the_west_inner.login import Game_login
 from the_west_inner.misc_scripts import sleep_closest_town
+from the_west_inner.reports import Job_report_reward_data
+from the_west_inner.work import Work, get_closest_workplace_data
 from the_west_inner.work_job_data import WorkJobData
 from typing import Protocol
 
 from automation_scripts.pre_work_managers.pre_work_change_equip import PreWorkEquipChangerManager,EquipmentChangeCollection
+from automation_scripts.pre_work_managers.pre_work_change_equip import PreWorkEquipChangerManager,PreWorkEquipChanger
+from automation_scripts.pre_work_managers.mov_pre_work_managers import PreWorkMovementManager
+
 
 
 class RepeatableScript(Protocol):
     
     def __init__(self ,
-                 speed_equip : Equipment | None = None,
-                 work_eq_dict : dict[int,EquipmentChangeCollection] | None = None
+                 hp_equipment : Equipment | None = None,
+                 mov_manager : PreWorkMovementManager | None = None,
+                 pre_work_changer : PreWorkEquipChangerManager | None = None
                  ):
-        
-        self.work_eq_dict = work_eq_dict
-        self.speed_equip = speed_equip
+        self.hp_equipment = hp_equipment
+        self.pre_work_changer = pre_work_changer
+        self.mov_manager = mov_manager
     
     def can_run(self , game_classes : Game_classes) -> bool:
         pass
@@ -27,6 +37,70 @@ class RepeatableScript(Protocol):
     
     def run(self , game_classes : Game_classes):
         pass
+
+class MoneyGainScript():
+    
+    def __init__(self ,
+                 work_list : list[Work],
+                 motivation_consumable : int,
+                 recharge_usable_list : list[int],
+                 hp_equipment : Equipment | None = None,
+                 mov_manager : PreWorkMovementManager | None = None,
+                 pre_work_changer : PreWorkEquipChangerManager | None = None
+                 ):
+        
+        self.work_list = work_list
+        self.motivation_consumable = motivation_consumable
+        self.recharge_usable_list = recharge_usable_list
+        
+        self.hp_equipment = hp_equipment
+        self.pre_work_changer = pre_work_changer
+        self.mov_manager = mov_manager
+    
+    def can_run(self , game_classes : Game_classes) -> bool:
+        
+        return not game_classes.work_manager.task_queue.sleep_task_in_queue() or game_classes.player_data.energy == game_classes.player_data.energy_max
+    
+    
+    def run(self , game_classes : Game_classes) -> Job_report_reward_data:
+        
+        cycle = Cycle_jobs(
+            game_classes=game_classes,
+            job_data=self.work_list,
+            consumable_handler=game_classes.consumable_handler,
+            
+        )
+        
+        stop_work_chain = CallbackChainer()
+        stop_work_chain.add_callback(
+            callback_function=stop_works,
+            work_manager = game_classes.work_manager
+        )
+        
+        
+        chain = CallbackChainer()
+
+        chain.add_callback(
+        callback_function=recharge_health_equipment,
+        handler = game_classes.handler ,
+        player_data = game_classes.player_data,
+        work_manager = game_classes.work_manager,
+        consumable_handler = game_classes.consumable_handler,
+        recharge_hp_consumable_id = self.recharge_usable_list,
+        equipment_manager = game_classes.equipment_manager,
+        hp_equipment = self.hp_equipment,
+        bag = game_classes.bag ,
+        stop_event_callable = stop_work_chain.chain_function()
+        )
+        
+        cycle.update_work_callback_chainer(callback_chain=chain)
+        
+        return cycle.cycle(
+            motivation_consumable=self.motivation_consumable,
+            energy_consumable=0,
+            number_of_cycles=1
+        )
+        
 
 class RegenerationManager:
     
@@ -104,3 +178,4 @@ class MoneyScript:
         while True:
             self._run()
             time.sleep(600)
+            self.delete_game_classes()
---
+++ automation_scripts/money_gain_script/money_script_build.py
@@ -0,0 +1,89 @@
+from automation_scripts.money_gain_script.money_script import MoneyGainScript,MoneyScript,RegenerationManager
+from automation_scripts.pre_work_managers.mov_pre_work_loader import PreWorkMovManagerBuilder
+from automation_scripts.pre_work_managers.mov_pre_work_managers import PreWorkMovementManager
+from automation_scripts.pre_work_managers.pre_work_change_equip import EquipmentChangeCollection, PreWorkEquipChangerManager
+from the_west_inner.equipment import Equipment, SavedEquipmentManager
+from the_west_inner.game_classes import Game_classes
+from the_west_inner.login import Game_login
+from the_west_inner.work import Work, get_closest_workplace_data
+
+
+
+def make_job_list(work_id_list : list[int] , game_classes : Game_classes) -> list[Work]:
+    job_data = []
+    for work_id in work_id_list:
+        job_id , x ,y = get_closest_workplace_data(
+        handler= game_classes.handler,
+        job_id= work_id,
+        job_list = game_classes.work_list,
+        player_data= game_classes.player_data
+        )
+        job_data.append(Work(job_id=job_id,x=x,y=y,duration=15))
+    return job_data
+
+
+def make_money_gain_script(
+                login : Game_login,
+                work_id_list : list[int],
+                hp_equipment : Equipment,
+                regeneration_equip : Equipment,
+                motivation_consumable : int,
+                recharge_usable_list : list[int],
+                town_id : int | None = None,
+                movement_equipment : None | Equipment = None,
+                work_eq_dict : dict[int,EquipmentChangeCollection] | None = None
+                ) -> MoneyScript:
+    
+    game = login.login()
+    
+    regeneration_manager = RegenerationManager(
+        regeneration_equip = regeneration_equip,
+        speed_equip= movement_equipment,
+        town_id = town_id
+    )
+    
+    job_data = make_job_list(work_id_list=work_id_list , game_classes=game)
+    
+    if movement_equipment is not None :
+        pre_work_movement_manager_builder = PreWorkMovManagerBuilder(game_classes=game)
+        pre_work_movement_manager = pre_work_movement_manager_builder.build(
+            work_list=job_data,
+            movement_equipment = movement_equipment
+        )
+    else:
+        pre_work_movement_manager = None
+    
+    if work_eq_dict is not None :
+        
+        saved_equipment_manager = SavedEquipmentManager(
+            handler = game.handler,
+            bag= game.bag
+        )
+        
+        
+        for job_work_equipment in work_eq_dict.values():
+            if not job_work_equipment.loaded :
+                job_work_equipment.load(saved_equip_manager=saved_equipment_manager)
+            
+    
+        pre_work_change_manager = PreWorkEquipChangerManager(
+                                        work_equipment_table = work_eq_dict,
+                                        handler = game.handler,
+                                        equipment_manager= game.equipment_manager
+                                        )
+    
+    money_gain_script = MoneyGainScript(
+        work_id_list = work_id_list,
+        motivation_consumable= motivation_consumable,
+        recharge_usable_list= recharge_usable_list,
+        hp_equipment= hp_equipment,
+        mov_manager= pre_work_movement_manager,
+        pre_work_changer= pre_work_change_manager
+        
+    )
+    
+    return MoneyScript(
+        login = login,
+        regeneration_manager= regeneration_manager,
+        repeatable_script= money_gain_script
+    )

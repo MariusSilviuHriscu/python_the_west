cfcc18d2c6f03121c04a2c5ee1adbeea8180d6c2
MariusSilviuHriscu <hriscumarius65536@gmail.com>
2025-02-11 14:06:52 +0200

    duel result and cycle

--- the_west_inner/duels.py
+++ the_west_inner/duels.py
@@ -6,6 +6,7 @@ import datetime
 
 from the_west_inner.requests_handler import requests_handler
 from the_west_inner.equipment import Equipment_manager,Equipment
+from the_west_inner.misc_scripts import turn_game_time_to_datetime
 from the_west_inner.player_data import Player_data
 
 STANCE_NUM : int = 4
@@ -93,6 +94,45 @@ class PlayerDuelStance():
             stance = stance
         )
 
+class DuelResultData:
+    
+    def __init__(self , 
+                 won : bool,
+                 ko : bool,
+                 experience : int,
+                 money : int,
+                 attacker_dead : bool):
+        
+        self.won = won
+        self.ko = ko
+        self.experience = experience
+        self.money = money
+        self.attacker_dead = attacker_dead
+    
+    @staticmethod
+    def load_from_dict(result_dict : dict[str,typing.Any] , attacker_dead : bool) -> typing.Self:
+        
+        result_dict = result_dict.get('resultData')
+        
+        won = result_dict.get('won')
+        ko = result_dict.get('ko') == 'def'
+        experience = result_dict.get('winner').get('xp') if won else 0
+        money = result_dict.get('winner').get('dollars') if won else 0
+        
+        return DuelResultData(
+            won = won,
+            ko=ko,
+            experience = experience,
+            money = money,
+            attacker_dead = attacker_dead
+        )
+    def __str__(self):
+        
+        return f'Won: {self.won}, KO: {self.ko}, Experience: {self.experience}, Money: {self.money}, Attacker Dead: {self.attacker_dead}'
+    
+    def __repr__(self):
+        
+        return self.__str__()
 
 @dataclass
 class DuelNpcData:
@@ -131,24 +171,31 @@ class NpcDuelList :
                  npc_duel_motivation : float ,
                  difficulty : int, 
                  npc_list : list[DuelNpcData],
-                 remaining_time : float
+                 remaining_time : float,
+                 arrival_delay : float | int = 0
                  ):
         
         self._npc_duel_motivation = npc_duel_motivation
         self._difficulty = difficulty
         self._npc_list = npc_list
+        self.arrival_delay = arrival_delay
         self.end_time = datetime.datetime.now() + datetime.timedelta(seconds=remaining_time)
+        self.arrival_delay = arrival_delay
     def _reload_data(self,handler:requests_handler) :
         
         response = handler.post(window='duel',action='reload',use_h=True)
         
         difficulty = response.get('npcs',{}).get('difficulty')
+        arrival_delay = response.get('npcs',{}).get('arrival_delay')
+        
         npc_data_dict = response.get('npcs').get('npcs')
+        remaining_time = response.get('npcs').get('remaining_time')
         
         self.__init__(npc_duel_motivation=self._npc_duel_motivation,
                       difficulty = difficulty ,
-                      npc_list = [DuelNpcData.load_from_dict(npc_data_dict=x) for x in npc_data_dict.values()],
-                      remaining_time = int(response)
+                      npc_list = [DuelNpcData.load_from_dict(npc_data_dict=x) for x in npc_data_dict],
+                      remaining_time = int(remaining_time),
+                    arrival_delay = arrival_delay
                       )
     
     def __contains__(self, npc_id:int):
@@ -175,7 +222,9 @@ class NpcDuelList :
             self._reload_data(handler = handler)
         
         return self._difficulty
-    
+    def get_npc_by_id(self,npc_id:int) -> DuelNpcData:
+        
+        return next((npc for npc in self._npc_list if npc.duelnpc_id == npc_id),None)
     def get_npc_by_smallest(self, key: str) -> int:
         # Create a dictionary with NPC IDs as keys and corresponding values for the specified key
         value_dict = {npc.duelnpc_id: getattr(npc, key) for npc in self._npc_list}
@@ -218,30 +267,50 @@ class NpcDuelList :
         
         return response
     
-    def duel_npc(self, npc_id : int,handler : requests_handler):
+    def duel_npc(self, npc_id : int,handler : requests_handler) -> DuelResultData:
         
         if npc_id not in self:
             raise ValueError('This npc is not available for duel!')
         
-        return self._duel_npc(handler = handler,npc_id = npc_id)
-
-    def yield_npcs_by_arrival(self) -> typing.Generator[int, None, None]:
-        """Yields NPC IDs in order of their arrival time.
-        - NPCs that are already available are yielded first.
-        - If no more NPCs are available, it waits for the next NPC.
-        """
-
-        # Sort NPCs by arrival time (earliest first)
-        sorted_npcs = sorted(self._npc_list, key=lambda npc: npc.arrival)
-
-        for npc in sorted_npcs:
-            wait_time = npc.arrival - datetime.datetime.now().timestamp()
-
-            if wait_time > 0:
-                # If the NPC hasn't arrived yet, wait until it becomes available
-                time.sleep(wait_time)
-
-            yield npc.duelnpc_id  # Yield the NPC ID when it's available
+        data = self._duel_npc(handler = handler,npc_id = npc_id)
+        
+        return DuelResultData.load_from_dict(result_dict = data.get('data') , attacker_dead = data.get('attacker_died',False))
+    
+    def is_ready(self, npc_id: int) -> bool:
+        
+        return self.arrival_delay - self.get_npc_by_id(npc_id).arrival_delta < 0
+    
+    def get_npc_arrival_time(self, npc_id: int) -> int | float | None:
+        
+        if self.is_ready(npc_id):
+            return None
+        
+        return self.arrival_delay - self.get_npc_by_id(npc_id).arrival_delta
+    
+    def wait_for_npc(self) -> None:
+        
+        smallest_delta = min([self.arrival_delay - npc.arrival_delta for npc in self._npc_list])
+        
+        time.sleep(smallest_delta)
+        time.sleep(1)
+        
+    
+    def yield_npcs_by_arrival(self , handler : requests_handler) -> typing.Generator[DuelResultData, None, None]:
+        while True:
+            available_npcs_ids = [npc.duelnpc_id for npc in self._npc_list if self.is_ready(npc.duelnpc_id)]
+            
+            if len(available_npcs_ids) == 0:
+                print('waiting for npc')
+                self.wait_for_npc()
+                self._reload_data(handler = handler)
+                continue
+                
+            
+            for npc_id in available_npcs_ids:
+                yield npc_id
+            
+            self._reload_data(handler = handler)
+        
 
 def get_npc_duel_data(handler:requests_handler) -> NpcDuelList:
     
@@ -256,12 +325,14 @@ def get_npc_duel_data(handler:requests_handler) -> NpcDuelList:
     difficulty = npc_data['difficulty']
     npc_dict = npc_data['npcs']
     remaining_time = float(npc_data['remaining_time'])
+    arrival_delay = npc_data['arrival_delay']
     
     return NpcDuelList(
         npc_duel_motivation = motivation_npc,
         difficulty = difficulty,
         npc_list = [DuelNpcData.load_from_dict(npc_data_dict=x) for x in npc_dict],
-        remaining_time = remaining_time
+        remaining_time = remaining_time,
+        arrival_delay = arrival_delay
     )
 
 class NpcDuelManager():
@@ -322,3 +393,12 @@ class NpcDuelManager():
         if self.npc_list.has_to_reload():
             
             self.npc_list._reload_data(handler= self.handler)
+    
+    def cycle_duels(self):
+        
+        for npc_id in self.npc_list.yield_npcs_by_arrival(handler=self.handler):
+            
+            if not self.can_afford:
+                break
+            
+            yield self.duel_npc(npc_id=npc_id)

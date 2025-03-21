ccff6a734842aadead2dc9e9f22666d7cd411a5a
MariusSilviuHriscu <89008442+MariusSilviuHriscu@users.noreply.github.com>
2025-03-21 14:34:14 +0200

    repaired function

--- automation_scripts/sleep_func_callbacks/play_tombola.py
+++ automation_scripts/sleep_func_callbacks/play_tombola.py
@@ -2,16 +2,33 @@ from the_west_inner.requests_handler import requests_handler
 
 
 def try_play_tombola(handler : requests_handler):
-    payload = {
-        'payid' :  1 ,
-        'cardid' :  4 ,
-        'gametype' :  'wheel_of_fortune' ,
-        'action' :  'main' ,
-        'wofid' :  1
-    }
-    handler.post(
+    data = handler.post(
         window='wheeloffortune',
-        action='gamble',
-        payload=payload,
-        use_h= True
+        action='init',
+        action_name='mode',
+        payload= {
+            'wofid': '1'
+        }
     )
+    
+    if data.get('error',False):
+        
+        return
+    
+    free_plays = data.get('mode').get('free')
+    if free_plays == 0:
+        return
+    for _ in range(free_plays):
+        payload = {
+            'payid' :  1 ,
+            'cardid' :  4 ,
+            'gametype' :  'wheel_of_fortune' ,
+            'action' :  'main' ,
+            'wofid' :  1
+        }
+        handler.post(
+            window='wheeloffortune',
+            action='gamble',
+            payload=payload,
+            use_h= True
+        )

1f08be9c8a122bd86340df6932456e34f0d3327b
MariusSilviuHriscu <hriscumarius65536@gmail.com>
2025-10-07 22:41:31 +0300

    Add NotebookEquipSaver for equipment state storage
    Introduces the NotebookEquipSaver class to save and retrieve current equipment state using a notebook. Includes methods to serialize equipment to JSON and restore it from notebook content.

---
+++ automation_scripts/notebook_storage/notebook_equipment.py
@@ -0,0 +1,31 @@
+import json
+
+from the_west_inner.equipment import Equipment_manager,Equipment
+from the_west_inner.notebook import Notebook
+
+class NotebookEquipSaver:
+    
+    def __init__(self,
+                 equipment_manager : Equipment_manager ,
+                 notebook : Notebook
+    ):
+        self.equipment_manager = equipment_manager
+        self.notebook = notebook
+    
+    def save_current_equipment(self ):
+        
+        current_equipment = {x:y for x,y in self.equipment_manager.current_equipment}
+        equipment_json = json.dumps(current_equipment)
+        
+        self.notebook.edit(new_text = equipment_json)
+    
+    def get_notebook_equipment(self) -> Equipment | None:
+        
+        equipment_str = self.notebook.content
+        
+        try:
+            return Equipment(
+                ** json.loads(equipment_str)
+            )
+        except json.JSONDecodeError as e:
+            return None
